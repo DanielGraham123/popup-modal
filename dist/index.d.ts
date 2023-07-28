c772636e67028a1ad0d527215f9a66e6145ff7ec
DanielGraham123 <dg.danielboaz@yahoo.com>
2023-07-28 18:48:55 +0100

    removed dist completely

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import './index.css';
-interface PopupModalProps {
-    children: React.ReactNode;
-    handleClose: () => void;
-}
-export declare const PopupModal: ({ children, handleClose }: PopupModalProps) => React.JSX.Element;
-export {};
